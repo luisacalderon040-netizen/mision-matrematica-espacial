75b8e43fe3798cb29a6bd579019b5f9ac598aaab
luisacalderon040-netizen <luisacalderon040@gmail.com>
2026-08-10 23:51:32 -0600

    Update page reference for Multiplicación

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ const PLANETAS_DATA = [
     { id: 4, name: "El valor posicional", emoji: "🟢", page: "mission4.html" },
     { id: 5, name: "Comparación y Redondeo", emoji: "🔴", page: "mission5.html" },
     { id: 6, name: "Resta espacial", emoji: "🟣", page: "mission6.html" },
-    { id: 7, name: "Multiplicación", emoji: "🟠", page: "#" },
+    { id: 7, name: "Multiplicación", emoji: "🟠", page: "mission7.html" },
     { id: 8, name: "División", emoji: "🟤", page: "#" },
     { id: 9, name: "Medidas y formas", emoji: "⚪", page: "#" },
     { id: 10, name: "Desafío espacial", emoji: "🌟", page: "#" }
